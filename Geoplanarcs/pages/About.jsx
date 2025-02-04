e25b2c03c7ad3e7ed98bcc6f11b00723897ba577
Mustapha Osman <wnfynn@gmail.com>
2025-02-04 17:31:25 +0000

    done with vision

--- Geoplanarcs/pages/About.jsx
+++ Geoplanarcs/pages/About.jsx
@@ -13,9 +13,30 @@ const About = () => {
           About Us
         </h1>
       </div>
-      <div>
-        <div>
-          <img src="/" alt="" />
+      <div className="flex border-2 border-amber-400 py-[5rem] max-md:flex-col items-center mb-[5rem] justify-center lg:px-[10%] max-md:px-[5%]  gap-10">
+        <div className="flex relative border-2 border-red-500">
+          <img
+            className="w-[50%]   rounded-tr-[40%] rounded-bl-[40%] "
+            src="/vs4.jpg"
+            alt=""
+          />
+          <img
+            className="w-[50%]  rounded-tl-[40%] rounded-br-[40%]"
+            src="/vs3.jpg"
+            alt=""
+          />
+        </div>
+        <div className="border-2">
+          <h3 className="text-green-500 xl:text-[2.8rem] lg:text-[2.4rem] sm:text-[2rem] max-sm:text-[1.5rem] font-semibold">
+            Vision
+          </h3>
+          <p>
+            At Geoplanarcs, we are dedicated to transforming visions into
+            reality. With a team of experienced professionals, we have
+            successfully executed numerous projects in the architectural and
+            real estate domain, earning a reputation for innovation and
+            excellence.
+          </p>
         </div>
       </div>
     </div>
